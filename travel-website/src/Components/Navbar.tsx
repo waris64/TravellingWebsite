81f62edcf0abca2c973e88632abefe68014105ca
waris@64 <warismuhammad652@gmail.com>
2025-07-12 16:05:58 +0500

    migration: Shifted form custom css to tailwind for productivity

--- travel-website/src/Components/Navbar.tsx
+++ travel-website/src/Components/Navbar.tsx
@@ -1,34 +1,50 @@
 import { Component } from "react";
-import "./NavbarStyles.css";
 import { MenuItems } from "./MenuItems";
+
 class Navbar extends Component {
   state = { clicked: false };
-  handleClick = () => {
-    this.setState({ clicked: !this.state.clicked });
-  };
-  render() {
-    return (
-      <nav className="NavbarItems">
-        <h1 className="Navbar-logo">Trippy</h1>
-        <div className="menu-icons" onClick={this.handleClick}>
-          <i
-            className={this.state.clicked ? "fa fa-times" : "fa fa-bars"}
-          ></i>
-        </div>
-        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu "}>
-          {MenuItems.map((item: any, index: any) => {
-            return (
-              <li key={index}>
-                <a href={item.url} className={item.cName}>
-                  <i className={item.icon}></i> {item.title}
-                </a>
-              </li>
-            );
-          })}
 
+  handleClick = () => {
+    this.setState((prev) => ({ clicked: !prev.clicked }));
+  };
+
+  render() {
+    const { clicked } = this.state;
+
+    return (
+      <nav className="fixed top-[5%] left-1/2 transform -translate-x-1/2 w-[95%] max-w-[1200px] h-[70px] px-4 flex justify-between items-center bg-white shadow-md rounded-[13px] z-50">
+        {/* Logo */}
+        <h1 className="text-xl font-bold text-[#01959a]">Trippy</h1>
+
+        {/* Hamburger Icon */}
+        <div
+          className="text-2xl md:hidden cursor-pointer z-50"
+          onClick={this.handleClick}
+        >
+          <i className={clicked ? "fa fa-times" : "fa fa-bars"}></i>
+        </div>
+
+        {/* Nav Menu */}
+        <ul
+          className={`absolute left-0 top-[70px] w-full bg-white flex flex-col items-center gap-2 transition-all duration-300 ease-in-out md:static md:flex-row md:w-auto md:bg-transparent md:gap-4 ${
+            clicked ? "opacity-100 visible" : "opacity-0 invisible md:visible md:opacity-100"
+          }`}
+        >
+          {MenuItems.map((item, index) => (
+            <li key={index} className="w-full md:w-auto text-center">
+              <a
+                href={item.url}
+                className="block w-full px-4 py-2 text-sm md:text-base font-semibold text-black hover:bg-[#01959a] hover:text-white transition-colors duration-200 rounded"
+              >
+                <i className={`${item.icon} mr-2`}></i>
+                {item.title}
+              </a>
+            </li>
+          ))}
         </ul>
       </nav>
     );
   }
 }
+
 export default Navbar;
