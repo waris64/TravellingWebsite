c55cd70cb3870782e7e8a8256a33d55fb4785932
waris@64 <warismuhammad652@gmail.com>
2025-07-14 18:36:34 +0500

    Development: Contact form added and responsive

--- travel-website/src/Routes/Contact.jsx
+++ travel-website/src/Routes/Contact.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from '../Components/Navbar';
 import Hero from '../Components/Hero';
+import ContactForm from '../Components/ContactForm';
+import Footer from '../Components/Footer';
 const Contact =()=>{
 return (
     <main>
          <Navbar/>
          <Hero heroImg="/assets/2.jpg" title="Contact Us" />
+         <ContactForm/>
+         <Footer/>
     </main>
 )
 } 
